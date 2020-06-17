e7ab929c7a210629a89cf2ccbe7850d28aa21c18
sunyutong <18147508318@163.com>
2020-06-17 10:09:22 +0800

    shaya

---
+++ src/components/commonIntroduce.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="xy2d_introduce">
+        <div class="introduce_cover">
+            <!-- 产品介绍的图片容器 -->
+            <slot></slot>
+        </div>
+        <div class="bottomBar">
+            <button class="bottom_btn1" :style="{backgroundColor:btnColor.wtbtn }" @click="leadgoSelfWrite">网投出单</button>
+            <button class="buttom_btn2" :style="{backgroundColor:btnColor.ytbtn}" @click="goInForm">易投出单</button>
+        </div>
+        <div class="loading-div" v-if="loadingFlag">
+            <loading/>
+        </div>
+        <Alert></Alert> 
+    </div>
+</template>
+
+<script>
+export default {
+
+} 
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/San/sqbb_introduce.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <commonIntroduce
+        :produceName="produceName"
+        :planCode="planCode"
+        :goPath="goPath"
+        :shareInfo="shareInfo"
+        :btnColor="btnColor"
+        >
+            <div class="xy2d_banner">
+                <img src="" alt="">
+            </div>
+        </commonIntroduce>
+    </div>  
+</template>
+
+<script>
+import commonIntroduce from '@components/commonIntroduce.vue'
+export default {
+    data(){
+        return{
+            produceName:"神奇大宝贝",//险种姓名
+            planCode:"B54",//险种码
+            goPath:'/ts_prpo/sqpp',//录单页面路径
+            shareInfo:{
+                shareTitle:"神奇大宝贝",//分享得title
+                shareImg:'',//分享图片路径
+                sampleIntroduce:'【重磅上市】多么完美可爱的商品啊，我是可爱本爱'
+            },
+            btnColor:{
+                wtBth:'#bccef8',
+                ytBth:'#8fb1fe'
+            }
+        }
+    },
+    components:{
+        commonIntroduce
+        }
+}
+</script>
+
+<style>
+
+</style>
