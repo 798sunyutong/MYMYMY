f08ba451679cfa140424c7366c934e6d60e5608e
sunyutong <18147508318@163.com>
2020-11-16 16:10:57 +0800

    for循环与定时器产生的问题

--- src/pages/Si/erzi.vue
+++ src/pages/Si/erzi.vue
@@ -2,6 +2,7 @@
   <div>
       <div>我是你儿子想要得到的大宝贝</div>
       <router-link to="/Si/fuzujian">我要回去</router-link>
+      <button @click="dsq()">定时器问题</button>
   </div>
 </template>
 
@@ -15,7 +16,41 @@ export default {
         }
     },
     methods: {
+        dsq(){
+            // 打印55555
+                // for(var i=0;i<5;i++){
+                //     setTimeout(() => {
+                //         console.log(i)
+                //     }, 1000);
+                // }
+            // 打印55555
+                // for(var i=0;i<5;i++){
+                //     setTimeout(() => {
+                //         console.log(i)
+                //     }, 1000,i);
+                // }
 
+            // 打印01234,let是块级作用域，var是全局变量
+            // for(let i=0;i<5;i++){
+            //     setTimeout(() => {
+            //         console.log(i)
+            //     }, 1000);
+            // }
+            // 打印01234，定时器放在自调用函数中，每次调用的时候会存储起来
+            // for(var i=0;i<5;i++){
+            //     (function fn(i){
+            //         setTimeout(() => {
+            //             console.log(i)
+            //         }, 1000)
+            //     })(i)
+            // }
+            // 打印01234,传入实参i，利用形参j来接收循环中每一次i的值
+            for(var i=0;i<5;i++){
+                setTimeout((j) => {
+                    console.log(j)
+                }, 1000,i)
+            }
+        },
     },
 
 }
